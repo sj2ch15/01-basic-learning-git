8e87031e173356c684b9eb007ad1375185ed409d
sj2ch15 <sj2ch15@gmail.com>
2022-04-09 22:38:42 +0530

    First spring demo project

---
+++ 02-springbootdemo-devtools/src/main/java/com/springbootdemo/myspringbootapp/rest/FunRestController.java
@@ -0,0 +1,22 @@
+package com.springbootdemo.myspringbootapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	// expose "/" that return hello world
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! the current time is "+LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Do a run for 5 KM daily!!!!";
+	}
+}
